8cb865325c89b7e431d57b6b6488ae38d62b28c4
fassbender1 <civello6969@gmail.com>
2026-02-22 00:05:53 +0200

    g

--- people/forms.py
+++ people/forms.py
@@ -1,14 +1,54 @@
 from django import forms
 
-from people.models import Actor
+from people.models import Actor, Writer, Director
 
 
 class ActorForm(forms.ModelForm):
     class Meta:
         model = Actor
-        fields = '__all__'
+        exclude = ['slug']
+        labels = {
+            'name': 'Actor Name',
+            'salary': 'Salary',
+            'nationality': 'Born in',
+            'agent_email': 'Agent Contact Info',
+        }
         error_messages = {
             'name': {
                 'unique': 'A person with this name already exists.'
+            },
+            'salary': {
+                'max_value': "An Actor's salary cannot exceed $50,000,000"
+            }
+        }
+
+class DirectorForm(forms.ModelForm):
+    class Meta:
+        model = Director
+        exclude = ['slug']
+        labes = {
+            'name': 'Director Name',
+            'salary': 'Salary',
+
+        }
+
+        error_messages = {
+            'salary': {
+                'max_value': "A Director's salary cannot exceed $15,000,000"
+            }
+        }
+
+class WriterForm(forms.ModelForm):
+    class Meta:
+        model = Writer
+        exclude = ['slug']
+        labes = {
+            'name': 'Writer Name',
+            'salary': 'Salary',
+        }
+
+        error_messages = {
+            'salary': {
+                'max_value': "A Writer's salary cannot exceed $5,000,000"
             }
         }
---
+++ people/mixin.py
@@ -0,0 +1,16 @@
+from django import forms
+
+
+class SalaryValidationMixin:
+    MAX_SALARY = None 
+
+    def clean_salary(self):
+        salary = self.cleaned_data.get("salary")
+
+        if salary and self.MAX_SALARY:
+            if salary > self.MAX_SALARY:
+                raise forms.ValidationError(
+                    f"Salary cannot exceed ${self.MAX_SALARY:,.0f}"
+                )
+
+        return salary
